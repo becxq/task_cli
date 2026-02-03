30f78262c36a9dc12643a481942d8696d792f7cd
becxq <s74042176@gmail.com>
2026-02-03 21:02:28 +0500

    start over programming controller

--- controller.go
+++ controller.go
@@ -6,37 +6,20 @@ import (
 
 type Status string
 
-const (
+const ( // enum для статуса
 	ToDo       Status = "todo"
 	InProgress Status = "in_progress"
 	Done       Status = "done"
 )
 
-type TaskLister struct {
+type TaskLister struct { // позволяет удобно взаимодействовать с tasks.json
 	Count int    `json:"count"`
-	Tasks []Task `json: "tasks`
+	Tasks []Task `json:"tasks`
 }
 
-type Task struct {
-	Id          int       `json:"tasks"`
-	Description string    `json:"tasks"`
-	Status      Status    `json:"tasks"`
-	CreatedAt   time.Time `json:"tasks"`
-	UpdatedAt   time.Time `json:"tasks"`
-}
-
-func (t Task) UpdateStatus(status Status) {
-
-}
-
-func (t Task) Show() (string, Status) {
-
-}
-
-func (t Task) Delete() {
-
-}
-
-func (t TaskLister) Create(description string, status Status) {
-
+type Task struct { // отдельный тип для задач
+	Description string    `json:"description"`
+	Status      Status    `json:"status"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
 }
--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
 
+func main() {
+	var taskmanager TaskLister
+
+	data, _ := os.ReadFile("tasks.json")
+	json.Unmarshal(data, &taskmanager)
+
+	fmt.Println(taskmanager.Count)
 }
